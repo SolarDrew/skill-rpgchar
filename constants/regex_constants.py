918de97e2d0c7e4d37c84bec646418e42cfb9b52
Drew Leonard <andy.j.leonard@gmail.com>
2018-05-15 08:37:04 +0100

    New useful verb in regex constants

--- constants/regex_constants.py
+++ constants/regex_constants.py
@@ -5,5 +5,8 @@ Definitions of regexes for matching common useful sentence parts.
 # Regex definitions
 OBJECT = '(?P<object>.*)' # in grammatical sense - person who is acting
 SUBJECT = '(?P<subject>.*)' # acted upon
+
 ATK_VERB = '(hits?|attacks?|swings? at)'
 WEAPON = '(?P<weapon>.*)'
+
+GAIN_VERB = '(gets?|gains?)'
