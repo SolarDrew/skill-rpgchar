55ca44f7cb1477f572a4c9f63126e1d775ce3cd2
Drew Leonard <andy.j.leonard@gmail.com>
2018-05-19 10:13:04 +0100

    Expand attack verb regex

--- constants/regex_constants.py
+++ constants/regex_constants.py
@@ -7,7 +7,7 @@ OBJECT = '(?P<object>\w+)' # in grammatical sense - person who is acting
 SUBJECT = '(?P<subject>\w+)' # acted upon
 POSSESSIVE = '(a|my|his|her|their)'
 
-ATK_VERB = '(hits?|attacks?|swings? at)'
+ATK_VERB = '(hits?|attacks?|swings? at|shoots?)'
 WEAPON = '(?P<weapon>\w+)'
 
 GAIN_VERB = '(gets?|gains?)'
